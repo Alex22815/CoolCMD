21072fdcd45fb5f01c1e7cc46e4526414c4534ea
Alex22815 <85880108+Alex22815@users.noreply.github.com>
2023-04-01 23:05:01 +0300

    Update StartOrUpdateCoolCMD.bat

--- StartOrUpdateCoolCMD.bat
+++ StartOrUpdateCoolCMD.bat
@@ -1,6 +1,7 @@
 @echo off
 Updating CoolCMD... Please, wait.
+
 curl https://raw.githubusercontent.com/Alex22815/CoolCMD/main/cmd.bat --output cmd.bat >nul
-Updating Updater... Please, wait.
+
 cls
 call "cmd.bat"
