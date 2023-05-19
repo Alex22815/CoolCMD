39bbe9d6ad2d492d9091b9a66cb31ef043748cf1
AlexzMilesz <85880108+Alex22815@users.noreply.github.com>
2023-05-19 23:07:30 +0300

    Update StartOrUpdateCoolCMD.bat

--- StartOrUpdateCoolCMD.bat
+++ StartOrUpdateCoolCMD.bat
@@ -2,6 +2,7 @@
 echo. Updating CoolCMD... Please, wait.
 echo.
 curl https://raw.githubusercontent.com/Alex22815/CoolCMD/main/cmd.bat --output cmd.bat
+curl https://raw.githubusercontent.com/Alex22815/CoolCMD/main/games.bat --output games.bat
 echo.
 echo.
 start cmd.bat
