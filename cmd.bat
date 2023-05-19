581405e946ed74329318507c8d4893c4028bc456
AlexzMilesz <85880108+Alex22815@users.noreply.github.com>
2023-05-19 22:37:19 +0300

    Update cmd.bat
    просто добавил about команду

--- cmd.bat
+++ cmd.bat
@@ -16,7 +16,8 @@ echo. Executing commnads on start
 title Executing Start up Commands
 :: %~dp0
 :: You can add some your commands before cmd will start! Go ahead! (or use "call "filename.bat"")
-
+echo.
+echo. Executing StartUp commands...
 
 
 :: Here the end of startup commands! 
@@ -30,7 +31,7 @@ cls
 title CoolCMD by AlexMiles22815
 echo.
 echo.           Hello! 
-echo.  Welcome to CoolCMD v0.45
+echo.  Welcome to CoolCMD v0.46.2
 echo.
 
 
@@ -41,9 +42,13 @@ goto CMD
 :CMD
 set /p a="%username%>> "
 title Executing: %a%
-%a%
+if %a% ==about (
+goto about
+) else (
 title Executed: %a%
+%a%
 goto CMD
+)
 
 
 
@@ -51,7 +56,17 @@ goto CMD
 
 
 
-
+:about
+cls
+title About
+echo.
+echo. About:
+echo.
+echo. This command-line tool, developed by AlexMiles, allows users to modify and customize their command line by adding new commands. Whether you're a developer or a system administrator, this tool is a great way to enhance your productivity and streamline your workflow. With the ability to add new commands and customize your command line, you can tailor your experience to fit your specific needs. If you have any questions or feedback, feel free to reach out to the author on Discord at AlexMiles#3666.
+echo.
+pause
+echo.
+goto CMD
 
 
 
@@ -89,3 +104,9 @@ set AutoRefreshScripts == done
 goto VARs
 
 :Update
+
+
+:ex
+%a%
+title Executed: %a%
+goto CMD
